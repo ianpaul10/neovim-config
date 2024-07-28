01d14252ad927fab7b8403f92df34c3e3316b41a
Ian Paul <iandouglaspaul@gmail.com>
2024-07-28 18:21:46 +0700

    Add tree view and show hidden files

--- lua/kickstart/plugins/neo-tree.lua
+++ lua/kickstart/plugins/neo-tree.lua
@@ -22,4 +22,11 @@ return {
       },
     },
   },
+  filesystem = {
+    filtered_items = {
+      hide_dotfiles = false,
+      hide_gitignored = false,
+      hide_hiddenn = false,
+    },
+  },
 }
