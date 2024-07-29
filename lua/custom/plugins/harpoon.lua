507679a23fc2f2f67955f516b458e2c40f9ce954
Ian Paul <iandouglaspaul@gmail.com>
2024-07-29 11:36:02 +0700

    Switch harpoon cmds

--- lua/custom/plugins/harpoon.lua
+++ lua/custom/plugins/harpoon.lua
@@ -11,14 +11,14 @@ return {
   end,
   keys = {
     {
-      '<leader>A',
+      '<leader>a',
       function()
         require('harpoon'):list():append()
       end,
       desc = 'harpoon file',
     },
     {
-      '<leader>a',
+      '<leader>g',
       function()
         local harpoon = require 'harpoon'
         harpoon.ui:toggle_quick_menu(harpoon:list())
