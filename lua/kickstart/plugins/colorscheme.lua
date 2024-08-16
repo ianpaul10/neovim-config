ed7968c7a67afc16edeb5e94577759cc81dd5ece
Ian Paul <iandouglaspaul@gmail.com>
2024-08-16 09:18:12 +0700

    back to night owl. nightfox wasn't high contrast enough

--- lua/kickstart/plugins/colorscheme.lua
+++ lua/kickstart/plugins/colorscheme.lua
@@ -1,42 +1,42 @@
 -- Change the name of the colorscheme plugin below, and then
 -- change the command in the config to whatever the name of that colorscheme is.
 -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
--- return {
---   'oxfist/night-owl.nvim',
---   -- 'folke/tokyonight.nvim', -- If you want to use tokyonight, you can uncomment this line.
---   lazy = false, -- make sure we load this during startup if it is your main colorscheme
---   priority = 1000, -- make sure to load this before all the other start plugins
---   config = function()
---     -- load the colorscheme here
---     require('night-owl').setup {
---       italics = false,
---       bold = true,
---       undercurl = true,
---       underline = true,
---       transparent_background = false,
---     }
---     vim.cmd.colorscheme 'night-owl'
---     -- vim.cmd.colorscheme 'slate' -- If you want to use the default slate colorscheme, you can uncomment this line.
---   end,
--- }
-
 return {
-  'EdenEast/nightfox.nvim',
-  lazy = false,
-  priority = 1000,
+  'oxfist/night-owl.nvim',
+  -- 'folke/tokyonight.nvim', -- If you want to use tokyonight, you can uncomment this line.
+  lazy = false, -- make sure we load this during startup if it is your main colorscheme
+  priority = 1000, -- make sure to load this before all the other start plugins
   config = function()
-    require('nightfox').setup {
-      options = {
-        colorblind = {
-          enable = true,
-          severity = {
-            protan = 0.9,
-            deutan = 0.3,
-            tritan = 0.0,
-          },
-        },
-      },
+    -- load the colorscheme here
+    require('night-owl').setup {
+      italics = false,
+      bold = true,
+      undercurl = true,
+      underline = true,
+      transparent_background = false,
     }
-    vim.cmd.colorscheme 'nightfox'
+    vim.cmd.colorscheme 'night-owl'
+    -- vim.cmd.colorscheme 'slate' -- If you want to use the default slate colorscheme, you can uncomment this line.
   end,
 }
+
+-- return {
+--   'EdenEast/nightfox.nvim',
+--   lazy = false,
+--   priority = 1000,
+--   config = function()
+--     require('nightfox').setup {
+--       options = {
+--         colorblind = {
+--           enable = true,
+--           severity = {
+--             protan = 0.9,
+--             deutan = 0.3,
+--             tritan = 0.0,
+--           },
+--         },
+--       },
+--     }
+--     vim.cmd.colorscheme 'nightfox'
+--   end,
+-- }
